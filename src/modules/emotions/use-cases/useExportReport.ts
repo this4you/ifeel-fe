880a6cbd28ae84d2bde6c61b2a392cd9a2b8af46
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-24 10:18:42 +0300

    Added date and time to report name

--- src/modules/emotions/use-cases/useExportReport.ts
+++ src/modules/emotions/use-cases/useExportReport.ts
@@ -3,11 +3,13 @@ import { useLogger } from '@commons/use-cases/useLogger.ts';
 import { useDownloadFile } from '@commons/use-cases/useDownloadFile.ts';
 import { exportReportRest } from '../api/exportReportRest.ts';
 import { MimeFileTypes } from '@commons/models/MimeFileTypes.ts';
+import moment from 'moment';
 
 export const useExportReport = () => {
     const { showLoader, hideLoader } = useAppLoadingStore();
     const downloadFile = useDownloadFile();
     const log = useLogger();
+    const currentDateFormatted = moment().format('MM/DD/YYYY HH:mm');
 
     return async () => {
         try {
@@ -15,7 +17,7 @@ export const useExportReport = () => {
 
             const blob = await exportReportRest();
 
-            downloadFile(blob, 'I feel', MimeFileTypes.XLSX);
+            downloadFile(blob, `I feel ${currentDateFormatted}`, MimeFileTypes.XLSX);
         } catch (e) {
             log(e as Error, 'Error happens during export emotions report');
         } finally {
