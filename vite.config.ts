117c43e9374372ddc7169dbb1361cd61a7fee56d
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-24 19:04:15 +0300

    Try to fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ifeel/',
-  build: {
-    rollupOptions: {
-      output: {
-        dir: 'dist',
-        entryFileNames: 'ifeel.js',
-        assetFileNames: 'ifeel.css',
-        chunkFileNames: "ifeelChunk.js",
-        manualChunks: undefined,
-      }
-    }
-  },
+  base: '/',
   plugins: [react(), tsconfigPaths()],
 })
