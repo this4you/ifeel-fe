34d02a0cd5edaa8f76002e22054e13ff1faca4e8
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-24 18:44:54 +0300

    Added deploy logic fix

--- src/modules/auth/api/getTokensRest.ts
+++ src/modules/auth/api/getTokensRest.ts
@@ -1,11 +1,11 @@
-import { baseAxios } from '../../../commons/api/baseAxios.ts';
+import { baseAxios } from '@commons/api/baseAxios.ts';
 import { Tokens } from '../models/Tokens.ts';
 
 export const getTokensRest = async (code: string): Promise<Tokens> => {
     const tokensResponse = await baseAxios.get<Tokens>('/auth/google/grantcode', {
         params: {
             code: code,
-            redirect: 'http://localhost:5173' // move to env
+            redirect: import.meta.env.VITE_GOOGLE_REDIRECT_URL
         }
     });
 
