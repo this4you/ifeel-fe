3b1b8234015546ac839d24ec3490a6e4a252e3b0
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-24 19:12:55 +0300

    Try to fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/',
+  base: '/ifeel-fe/',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'ifeel-fe.js'
+      }
+    }
+  },
   plugins: [react(), tsconfigPaths()],
 })
