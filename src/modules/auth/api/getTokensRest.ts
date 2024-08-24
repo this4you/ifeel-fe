0eff7885393333b9c8cb5089d7457ba81df1a110
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-24 20:06:24 +0300

    Added logging

--- src/modules/auth/api/getTokensRest.ts
+++ src/modules/auth/api/getTokensRest.ts
@@ -2,7 +2,7 @@ import { baseAxios } from '@commons/api/baseAxios.ts';
 import { Tokens } from '../models/Tokens.ts';
 
 export const getTokensRest = async (code: string): Promise<Tokens> => {
-    console.log('GOOGLE_REDIRECT_URL', import.meta.env.VITE_GOOGLE_REDIRECT_URL);
+    console.log('GOOGLE_REDIRECT_URL_V2', import.meta.env.VITE_GOOGLE_REDIRECT_URL);
 
     const tokensResponse = await baseAxios.get<Tokens>('/auth/google/grantcode', {
         params: {
