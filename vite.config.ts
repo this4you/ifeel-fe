d70a799d0eae13c1bcddf792a13b7da799aa7669
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-24 18:56:34 +0300

    Fixed config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ifeel/',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'ifeel.js',
+        assetFileNames: 'ifeel.css',
+        chunkFileNames: "ifeelChunk.js",
+        manualChunks: undefined,
+      }
+    }
+  },
   plugins: [react(), tsconfigPaths()],
 })
